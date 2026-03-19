1e8b50818d8ad35e8108e2efddeba87553da0ff3
lkilpatrick <luke@lukek.ca>
2026-03-19 05:54:49 -0700

    Fix contact form FROM_EMAIL: use luke@pitterpatterdiving.com (verified sender)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@ const mailgunApiKey = defineSecret("MAILGUN_API_KEY");
 
 const MAILGUN_DOMAIN = "pitterpatterdiving.com";
 const TO_EMAIL = "luke@lukek.ca";
-const FROM_EMAIL = `contact@${MAILGUN_DOMAIN}`;
+const FROM_EMAIL = `luke@${MAILGUN_DOMAIN}`;
 const MAILGUN_URL = `https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/messages`;
 
 export const sendContactEmail = onRequest({ secrets: [mailgunApiKey], invoker: "public" }, async (req, res) => {
