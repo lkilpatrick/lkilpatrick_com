68681e87d1b4e17274038d05c3ac4c199519796a
lkilpatrick <luke@lukek.ca>
2026-03-19 05:30:57 -0700

    Remove old route pages (/projects /docs /social /video) - site is new, no indexing

--- app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function ProjectsRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace("/work"); }, [router]);
-  return null;
-}
--- app/video/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-export default function VideoRedirect() {
-  const router = useRouter();
-  useEffect(() => { router.replace("/content#presentations"); }, [router]);
-  return null;
-}
